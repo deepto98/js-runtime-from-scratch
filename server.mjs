491832dcab7677a776e98e95a58345ec0ff67583
deepto <ddepp98@outlook.com>
2023-02-16 14:54:52 +0530

    Added error handling

--- server.mjs
+++ server.mjs
@@ -1,7 +1,16 @@
 import { createServer } from "http";
 
 const PORT = 1337;
+//HTTP Server
 createServer((request, response) => {
   response.writeHead(200);
   response.end("Hello");
+//   throw new Error('Mock Error');
 }).listen(1337, () => console.log("Listening on port", PORT));
+
+// Error handling
+["uncaughtException", "unhandledRejection"].forEach((event) =>
+  process.on(event, (err) => {
+    console.error(`Event: ${event}, message: ${err.stack || err}`);
+  })
+);
